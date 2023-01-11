538bfbcc5663b5441d763160c3021e8fb2d51033
Felix-1871 <nilxdzk@gmail.com>
2023-01-11 23:29:32 +0100

    Revamped page

--- kontakt.php
+++ kontakt.php
@@ -6,14 +6,14 @@ session_start();
   <head>
           <meta charset="utf-8" />
           <title>Kino Bilecik</title>
-          <link rel="stylesheet" href="assets/css/style.css" />
           <link rel="stylesheet" href="css/fontello.css" />
+          <link rel="stylesheet" href="assets/css/contact.css">
            <link href="https://fonts.googleapis.com/css?family=Lato:400,900|Raleway:400,900|Shadows+Into+Light&amp;subset=latin-ext" rel="stylesheet" />
 
           </head>
   <body>
      <header>
-        <h1 class="loggoo"
+        <h1 class="logo"
 ><i class="icon-video-2"></i> Kino Bilecik</h1>
 
         <nav id="topnav">
@@ -33,25 +33,70 @@ session_start();
           </ul>
         </nav>
      </header>
-     <div class="content">
-     <main>
-     <header>
-        <h2>Kontakt</h2>
 
-      </header>
-      <div class="contact">
-          <div class="in">
-            <i class="icon-user"></i>
-            <span id="in">Linus Torvalds</span>
-          </div>
-          <div class="mail">
-            <i class="icon-mail"></i>
-            <span id="mail">linus.torvalds@gmail.com</span>
-          </div>
-      </div>
+      <div id="text"><div id="text">
+  <div class="line">
+    <p class="word">Transgender</p>
+  </div>
 
-     </main>
-     </div>
+  <div class="line">
+    <p class="word">Girl</p>
+    <p class="word">&</p>
+  </div>
+
+  <div class="line">
+    <p class="word">Programmer</p>
+  </div>
+  
+  <div class="line">
+    <a 
+       id="channel-link" 
+       href="https://github.com/Felix-1871" 
+       target="_blank" 
+       class="word fancy"
+    >
+      @Feli_x
+    </a>
+  </div>
+</div>
+<script>
+  const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const enhance = id => {
+  const element = document.getElementById(id),
+        text = element.innerText.split("");
+  
+  element.innerText = "";
+  
+  text.forEach((value, index) => {
+    const outer = document.createElement("span");
+    
+    outer.className = "outer";
+    
+    const inner = document.createElement("span");
+    
+    inner.className = "inner";
+    
+    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+    
+    const letter = document.createElement("span");
+    
+    letter.className = "letter";
+    
+    letter.innerText = value;
+    
+    letter.style.animationDelay = `${index * 1000 }ms`;
+    
+    inner.appendChild(letter);    
+    
+    outer.appendChild(inner);    
+    
+    element.appendChild(outer);
+  });
+}
+
+enhance("channel-link");
+</script>
 
     <footer>
       <div class="socials">
