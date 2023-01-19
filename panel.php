051e7385ea4eb647572e02209625074de0a022fe
Felix-1871 <nilxdzk@gmail.com>
2023-01-19 20:58:35 +0100

    new panel

--- panel.php
+++ panel.php
@@ -1,79 +1,68 @@
-<?php
-session_start();
-$db = new PDO('sqlite:users.db');
-
-if(!isset($_SESSION['user']))
-{header('location:logowanie.php');
-exit;
-}
-
-?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-          <meta charset="utf-8" />
-          <title>Kino Bilecik</title>
-          <link rel="stylesheet" href="assets/css/style.css" />
-          <link rel="stylesheet" href="css/fontello.css" />
-           <link href="https://fonts.googleapis.com/css?family=Lato:400,900|Raleway:400,900|Shadows+Into+Light&amp;subset=latin-ext" rel="stylesheet" />
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="assets/css/panel.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-          </head>
-  <body>
-     <header>
-        <h1 class="logo"
-><i class="icon-video-2"></i> Kino Bilecik</h1>
+</head>
+<body>
+   
+  <input type="checkbox" id="check">
 
-        <nav id="topnav">
-          <ul class="menu">
-            <li><a href="index.php">Strona główna</a></li>
-            <li><a href="repertuar.php">Repertuar</a></li>
-            <li><a href="cennik.php">Cennik</a></li>
-            <li><a href="kontakt.php">Kontakt</a></li>
-            <li><a href="<?php if (!isset($_SESSION['user'])) {
-              print 'logowanie.php">Zaloguj się</a></li>';
-            } elseif (isset($_SESSION['isAdmin'])) {
-              print 'panel.php">Panel Administratora</a></li>';
-            } else {
-              print 'panel.php">Panel Użytkownika</a></li>';} ?>
-          </ul>
-        </nav>
-     </header>
-     <div class="content">
-     <main>
-      <?php
+  <label for="check">
+<i class="fa fa-bars" id="btn"></i>
+<i class="fa fa-times" id="cancle"></i>
 
-        print 'Co chcesz zrobić?: <br /> <br />';
-         print '<p><a href="edytrezerwacje.php" class="panel">Usuń rezerwację</a></p>'.PHP_EOL;
-         print '<p><a href="dodajseans.php" class="panel">Dodaj/usuń seanse</a></p>'.PHP_EOL;
-         
-         print '<br />';
-          print '<p><a href="wyloguj.php" class="button">Wyloguj się</a></p>'.PHP_EOL;
-      ?>
 
-  
-     </main>
-     </div>
-     
-    <footer>
-      <div class="socials">
-            <div class="fb">
-              <i class="icon-facebook-official"></i>
-            </div>
-            <div class="tw">
-              <i class="icon-twitter"></i>
-            </div>
-            <div class="insta">
-              <i class="icon-instagram"></i>
-            </div>            
-      </div> 
-
-      <div class="info">
-Zapraszamy na seanse w naszym kinie!
+  </label>
+  <div class="sidebar">
+<header>
+<img src="assets/images/i don't wanna do this anymore/profile-pic.png">
+<p>Administrator</p>
+</header>
+<ul>
+    <li><a href="#"><i class="fa fa-qrcode"></i>Dashboard</a></li>
+    <li><a href="#"><i class="fa fa-link"></i>Shortcuts</a></li>
+    <li><a href="#"><i class="fa fa-eye"></i>Overview</a></li>
+    <li><a href="#"><i class="fa fa-book"></i>Rezerwacje</a></li>
+    <li><a href="#"><i class="fa fa-eye"></i>Seanse</a></li>
+    <li><a href="#"><i class="fas fa-cog"></i>Service</a></li>
+    <li><a href="index.php"><i class="fas fa-user-shield"></i>Wyloguj się</a></li>
+    </ul>
+   <li>
+      <div class="social-links">
+        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-whatsapp"></i></a>
+        
       </div>
+    </li>
+   
 
-    </footer>
-  </body>
-  
+</div> 
+<h1>Wybierz aplikację:</h1>
+<div class="wrapper">
+ 
+<div id="grid-col">
+  <div class="cell">
+<h3>DEPL</h3>
+  </div> 
+  <div class="cell">
+<h3>oeu</h3>
+  </div>
+  <div class="cell">
+<h3>aoeu</h3>
+  </div>
+</div>
+</div>
+<div style="height: 80vmin;"></div>
+<footer>
+</footer>
+</body>
 </html>
-
